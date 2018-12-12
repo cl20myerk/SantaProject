84ebd5a9eb08314cec07339441fa1e410aef2800
Kyle <45437377+cl20myerk@users.noreply.github.com>
2018-12-12 12:31:39 -0500

    Add files via upload

--- SantaProject.java
+++ SantaProject.java
@@ -1,51 +1,91 @@
+/*
+Kyle Myer
+Dylan Tull
+Erin
+Chris Kramer
+Ronin
+*/
 import java.util.*;
-import java.io.FileNotFoundException;
 import java.io.*;
 
 public class SantaProject
 {
-	public static void main(String[] args) throws FileNotFoundException
+	static List<String> gList = new ArrayList<String>();
+	static List<String> kList = new ArrayList<String>();
+
+	public static void main(String[] args) throws IOException
 	{
-		//Get scanner instance
-		Scanner r = new Scanner(new File("kids.txt"));
-		ArrayList<String> niceListName = new ArrayList<String>();//create an array to act as the nice list name only
-		ArrayList<String> niceListAge = new ArrayList<String>();//array list (parallel) for nice peoples names
-		ArrayList<String> bassProShop = new ArrayList<String>();// array list to just catch all the info so I can sort it
-		String name, naughtyNice;
-		int age;
+		files();
+		giftsInfo();
+	}
 
-		//Set the delimiter used in file
-		r.useDelimiter(",");
+	public static void files() throws IOException
+	{
+		Scanner gifts = new Scanner(new FileReader("gifts.txt"));
+		Scanner kids = new Scanner(new FileReader("kids.txt"));
 
-		while (r.hasNext()) {
-			//System.out.println (r.next());
-			bassProShop.add(r.next());
+		String tmpG = "";
+		String tmpK = "";
+		String[] words = {""};
+
+		while(gifts.hasNext())
+		{
+			gList.add(gifts.nextLine());
 
 		}
-		r.close();// close scanner, just because
-
-		//prints out what net has to be sure
-
-		for (String elem : bassProShop) {
-			System.out.println(elem);
-		}
-
-
-		//searches throught the net to find nice people, adds their name and age to list
-		for (int x = 0; x < bassProShop.size(); x += 3) {
-			if (bassProShop.get(x + 1).equals(" nice")) {
-				niceListName.add(bassProShop.get(x));
-				niceListAge.add(bassProShop.get(x + 2));
+		while(kids.hasNext())
+		{
+			tmpK = kids.nextLine();
+			tmpK = tmpK.replaceAll("," , "\t");
+			tmpK = tmpK.replaceAll(" ", "");
+			words = tmpK.split(" ");
+			for(String s: words)
+			{
+				kList.add(s);
 			}
 		}
+		for(int i = 0; i < kList.size(); i++)
+		{
+			if(kList.get(i).contains("naughty"))
+			{
+				kList.remove(i);
+			}
+		}
+		//System.out.println(kList);
+	}
+	public static void giftsInfo() throws IOException
+	{
+		Scanner r = new Scanner(System.in);
+		int numDays = 0;
+		int num = 0;
+		List<Integer> index = new ArrayList<Integer>();
 
-		// print out the nice list
-		for (String s : niceListName)
-			System.out.println (s);
+		System.out.println("How many days do we have?");
+		numDays = r.nextInt();
 
-		for (String e : niceListAge)
-			System.out.println (e);
+		Scanner gifts = new Scanner(new FileReader("gifts.txt"));
 
+		List<String> name = new ArrayList<String>();
+		List<String> min = new ArrayList<String>();
+		List<String> max = new ArrayList<String>();
+		List<String> price = new ArrayList<String>();
+		List<String> days = new ArrayList<String>();
 
-    }
+		while(gifts.hasNext())
+		{
+			name.add(gifts.nextLine());
+			min.add(gifts.nextLine());
+			max.add(gifts.nextLine());
+			price.add(gifts.nextLine());
+			days.add(gifts.nextLine());
+		}
+		for(int i = 0;i < days.size(); i++)
+		{
+			num = Integer.parseInt(days.get(i));
+			if(num < numDays)
+			index.add(i);
+		}
+		System.out.println(index);
+
+	}
 }
